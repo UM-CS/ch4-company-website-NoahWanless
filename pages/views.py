5fba8224caae838ae13e14d4c034448443c420fa
Noah Wanless <noah@steveandtrena.com>
2025-09-30 20:18:43 -0600

    blank

---
+++ pages/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import render
+
+def home_page_view(request):
+	context = {
+		"inventory_list":["Widget 1","Widget 2","Widget 3"],
+		"greetings": "Thank you for visiting."
+
+	}
+	return render(request,"home.html",context )
+
+
+class AboutPageView(TemplateView):
+	template_name = "about.html" # the html to use in making the view
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["contact_address"] = "123 Main Street"
+		context["phone_number"] = "555-555-5555"
+		return context
